a0669106085237976e7c855b354f97ab05ab4fcb
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-07 15:12:22 +0800

    api shop/help更新

--- modules/shop/help_module.class.php
+++ modules/shop/help_module.class.php
@@ -5,7 +5,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author royalwang
  *
  */
-class detail_module extends api_front implements api_interface {
+class help_module extends api_front implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
     	
     	$this->authSession();
@@ -20,30 +20,8 @@ class detail_module extends api_front implements api_interface {
 }
 
 function get_shop_help2() {
-	$dbview = RC_Loader::load_app_model('article_viewmodel', 'article');
-	$res = $dbview->join('article_cat')
-				->where(array('ac.cat_type' => 5, 'ac.parent_id' => 3, 'a.is_open' => 1, 'a.open_type' => 0, 'cat_name is not null'))
-				->order(array('ac.sort_order' => 'ASC', 'a.article_id' => 'ASC'))
-				->select();
-	
-    $arr = array();
-	if (!empty($res)) {
-	    foreach ($res AS $key => $row) {
-		    if (!empty($row['link']) && $row['link'] != 'http://' && $row['link'] != 'https://') {
-				continue;
-			}
-			if (empty($row['content']) || empty($row['cat_name'])) {
-				continue;
-			}
-
-	        $arr[$row['cat_id']]['name']                     	 = $row['cat_name'];
-	        $arr[$row['cat_id']]['article'][$key]['id']  		 = $row['article_id'];
-	        $arr[$row['cat_id']]['article'][$key]['title']       = $row['title'];
-	        $arr[$row['cat_id']]['article'][$key]['short_title'] = ecjia::config('article_title_length') > 0 ?
-	        RC_String::sub_str($row['title'], ecjia::config('article_title_length')) : $row['title'];
-	    }
-	}
-	return $arr;
+	$data = RC_Model::model('article/article_viewmodel')->get_shop_help();
+	return $data;
 }
 
 // end
