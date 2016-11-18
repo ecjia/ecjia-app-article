e8f5e4d2f5b392ebe248d9772139e2d56d82353c
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 10:58:30 +0800

    文章相关接口增加缓存

---
+++ model/orm_article_model.class.php
@@ -0,0 +1,28 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+use Royalcms\Component\Database\Eloquent\Model;
+
+class orm_article_model extends Model {
+	protected $table = 'article';
+	
+	/* 获取缓存数据*/
+	public function get_cache_item ($cache_key) 
+	{
+		return RC_Cache::app_cache_get($cache_key, 'article');
+	}
+	
+	/* 设置缓存数据*/
+	public function set_cache_item ($cache_key, $item)
+	{
+		return RC_Cache::app_cache_set($cache_key, $item, 'article', 10080);
+	}
+	
+	/* 释放缓存数据*/
+	public function delete_cache_item ($cache_key)
+	{
+		return RC_Cache::app_cache_delete($cache_key, 'article');
+	}
+	
+}
+
+// end
--- modules/shop/help_module.class.php
+++ modules/shop/help_module.class.php
@@ -7,14 +7,19 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class help_module extends api_front implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    	
-    	$this->authSession();
-		$data = get_shop_help2();
-		$out = array();
-		foreach ($data as $value) {
-			$value['article'] && $value['article'] = array_values($value['article']);
-			$out[] = $value;
-		}
+    	$cache_article_key = 'article_list';
+    	$cache_id = sprintf('%X', crc32($cache_article_key));
+    	$article_db = RC_Model::model('article/orm_article_model');
+    	$out = $article_db->get_cache_item($cache_id);
+    	if (empty($out)) {
+    		$data = get_shop_help2();
+    		$out = array();
+    		foreach ($data as $value) {
+    			$value['article'] && $value['article'] = array_values($value['article']);
+    			$out[] = $value;
+    		}
+    		$article_db->set_cache_item($cache_id, $out);
+    	}
 		return $out;
 	}
 }
