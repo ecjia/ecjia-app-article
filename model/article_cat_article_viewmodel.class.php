1d486dffb7d72a385b7c8e83ac8daadcdfaae67f
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-06 15:30:30 +0800

    整理cart模块

--- model/article_cat_article_viewmodel.class.php
+++ model/article_cat_article_viewmodel.class.php
@@ -10,10 +10,10 @@ class article_cat_article_viewmodel extends Component_Model_View {
 
 		$this->view = array(
 			'article_cat' => array(
-				'type'  =>	Component_Model_View::TYPE_LEFT_JOIN,
-				'alias' => 	'b',
-				'field' => 	'a.cat_id, a.cat_name, a.sort_order AS parent_order, a.cat_id, b.cat_id AS child_id, b.cat_name AS child_name, b.sort_order AS child_order',
-				'on'    => 	'b.parent_id = a.cat_id',
+				'type'  => Component_Model_View::TYPE_LEFT_JOIN,
+				'alias' => 'b',
+				'field' => 'a.cat_id, a.cat_name, a.sort_order AS parent_order, a.cat_id, b.cat_id AS child_id, b.cat_name AS child_name, b.sort_order AS child_order',
+				'on'    => 'b.parent_id = a.cat_id',
 			)
 		);
 		parent::__construct();
