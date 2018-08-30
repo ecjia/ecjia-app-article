bcffc82c2946e7e01d961bad70605627094d62cd
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-31 00:01:25 +0800

    指定ServiceProvider的路径

--- classes/ArticleServiceProvider.php
+++ classes/ArticleServiceProvider.php
@@ -9,7 +9,7 @@ class ArticleServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-article', null, dirname(__DIR__));
+        $this->package('ecjia/app-article');
     }
     
     public function register()
