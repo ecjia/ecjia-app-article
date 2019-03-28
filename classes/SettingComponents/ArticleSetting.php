79037ec3ec55a16ab93c5eb2f8c103ebe78401d7
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-28 10:16:25 +0800

    更新文章模块的设置参数

--- classes/SettingComponents/ArticleSetting.php
+++ classes/SettingComponents/ArticleSetting.php
@@ -37,6 +37,9 @@ class ArticleSetting extends ComponentAbstract
     {
 
         $data = [
+            ['code' => 'article_title_length', 'value' => '20', 'options' => ['type' => 'text']],
+            ['code' => 'article_page_size', 'value' => '20', 'options' => ['type' => 'text']],
+            ['code' => 'help_open', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '0,1']],
 
         ];
 
@@ -46,6 +49,30 @@ class ArticleSetting extends ComponentAbstract
     public function getConfigs()
     {
         $config = [
+            [
+                'cfg_code' => 'article_title_length',
+                'cfg_name' => __('文章标题的长度', 'article'),
+                'cfg_desc' => '',
+                'cfg_range' => '',
+            ],
+
+            [
+                'cfg_code' => 'article_page_size',
+                'cfg_name' => __('文章分类页列表的数量', 'article'),
+                'cfg_desc' => '',
+                'cfg_range' => '',
+            ],
+
+            [
+                'cfg_code' => 'help_open',
+                'cfg_name' => __('用户帮助是否打开', 'article'),
+                'cfg_desc' => '',
+                'cfg_range' => array(
+                    '0' => __('关闭', 'goods'),
+                    '1' => __('打开', 'goods'),
+                ),
+            ],
+
 
         ];
 
